d512a0a81039a242054bbad2799b5333b0fb42cd
surya1243 <surya1243@gmail.com>
2024-06-26 01:09:23 +0545

    home page created for viewer

--- src/main/java/com/pofil/config/WebSecurityConfig.java
+++ src/main/java/com/pofil/config/WebSecurityConfig.java
@@ -53,7 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/","/registerviewer").permitAll()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/user/**").hasAuthority("USER")
                 .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
@@ -74,7 +74,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         web
                 .ignoring()
                 .antMatchers("/resources/**", "/static/**", "/font-awesome/**", "/fonts/**", "/css/**", "/js/**",
-        				"/images/**");
+        				"/images/**", "/video/**");
     }
 
 }
