b5d248f73bacf9a0dba805b6908dbd45d30dcc0d
surya1243 <surya1243@gmail.com>
2024-06-27 10:18:05 +0545

    redirect managed for login and signup

--- src/main/java/com/pofil/controller/LoginController.java
+++ src/main/java/com/pofil/controller/LoginController.java
@@ -3,14 +3,17 @@ package com.pofil.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-	@GetMapping({"/login" })
-	public String getLoginForm(HttpServletRequest request) {
+	@GetMapping({"/login","/signup" })
+	public String getLoginForm(HttpServletRequest request,@RequestParam(required = false) String action, Model model) {
 		if (request.getRemoteUser() != null)
 			return "redirect:/dashboard";
+		model.addAttribute("showSignup", "signup".equals(action));
 		return "login";
 	}
 	
