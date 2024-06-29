6fe2d0f07d611a76e3b29841187e8bf4da4ba13e
surya1243 <surya1243@gmail.com>
2024-06-29 16:22:11 +0545

    removed unused files

--- src/main/java/com/pofil/PofilApplication.java
+++ src/main/java/com/pofil/PofilApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.pofil.model.BankCardInfo;
 import com.pofil.model.Role;
-import com.pofil.repository.BankCardInfoRepository;
 import com.pofil.repository.RoleRepository;
 import com.pofil.service.CustomUserDetailsService;
 import com.pofil.service.FileSystemStorageService;
@@ -20,21 +18,20 @@ public class PofilApplication {
 	}
 	@Bean
 	CommandLineRunner init(RoleRepository roleRepository, FileSystemStorageService storageService, 
-			CustomUserDetailsService userDetailsService, BankCardInfoRepository bankCardInfoRepository) {
+			CustomUserDetailsService userDetailsService) {
 	    return args -> {
 	    	
 	    	storageService.init();
 	    	
-	    	BankCardInfo bankCardInfo = bankCardInfoRepository.findByInstitution("POFL");
-	    	if(bankCardInfo == null) {
-	    		BankCardInfo newBankCardInfo = new BankCardInfo();
-	    		newBankCardInfo.setBankName("Pokhara Finance Ltd.");
-	    		newBankCardInfo.setbIN("62347712");
-	    		newBankCardInfo.setOldBin("637029");
-	    		newBankCardInfo.setInstitution("POFL");
-	    		newBankCardInfo.setCardProduct("POFD");
-	    		bankCardInfoRepository.save(newBankCardInfo);
-	    	}
+			/*
+			 * BankCardInfo bankCardInfo = bankCardInfoRepository.findByInstitution("POFL");
+			 * if(bankCardInfo == null) { BankCardInfo newBankCardInfo = new BankCardInfo();
+			 * newBankCardInfo.setBankName("Pokhara Finance Ltd.");
+			 * newBankCardInfo.setbIN("62347712"); newBankCardInfo.setOldBin("637029");
+			 * newBankCardInfo.setInstitution("POFL");
+			 * newBankCardInfo.setCardProduct("POFD");
+			 * bankCardInfoRepository.save(newBankCardInfo); }
+			 */
 
 	    	 Role viewerRole = roleRepository.findByRole("VIEWER");
 		        if (viewerRole == null) {
--- src/main/java/com/pofil/controller/UtilityRestController.java
+++ src/main/java/com/pofil/controller/UtilityRestController.java
@@ -45,111 +45,24 @@ import com.pofil.model.ManagerCheque;
 import com.pofil.model.Minutes;
 import com.pofil.model.Sports;
 import com.pofil.model.UtilityBills;
-import com.pofil.repository.CardRenewRepository;
-import com.pofil.repository.CardRepository;
-import com.pofil.repository.FeedbackRepository;
-import com.pofil.repository.InsuranceCustomerRegisterRepository;
-import com.pofil.repository.InsuranceCustomerRepository;
-import com.pofil.repository.LockerLogRepository;
-import com.pofil.repository.MaintenanceLogRepository;
-import com.pofil.repository.MinutesRepository;
+
 import com.pofil.repository.UserRepository;
-import com.pofil.repository.UtilityBillsRepository;
-import com.pofil.service.AccountInfoDetailService;
-import com.pofil.service.BudgetDetailService;
-import com.pofil.service.CardDeliveryDetailService;
-import com.pofil.service.CardDetailService;
-import com.pofil.service.CardRenewDetailService;
-import com.pofil.service.ErrorIssueLogDetailService;
-import com.pofil.service.FeedbackDetailService;
-import com.pofil.service.FiscalYearDetailService;
-import com.pofil.service.GoodForPaymentDetailService;
-import com.pofil.service.InsuranceCompanyDetailService;
-import com.pofil.service.InsuranceCustomerRegisterDetailService;
-import com.pofil.service.InsuranceSchemaDetailService;
-import com.pofil.service.LockerDetailService;
-import com.pofil.service.LockerLogDetailService;
-import com.pofil.service.MaintenanceLogDetailService;
-import com.pofil.service.ManagerChequeDetailService;
 import com.pofil.service.SportsDetailService;
 import com.pofil.service.UserDetailService;
-import com.pofil.service.UtilityBillsDetailService;
-import com.pofil.util.Response;
 
 @RestController
 public class UtilityRestController {
 	@Autowired
 	MongoTemplate mongoTemplate;
-	@Autowired
-	private FeedbackDetailService feedbackDetailService;
-	
-	@Autowired
-	private MinutesRepository minutesRepository;
 	
 	@Autowired
 	private UserDetailService userDetailService;
-	
-	@Autowired
-	private InsuranceCustomerRegisterRepository insCustRegisterRepository;
 	@Autowired
 	private UserRepository userRepository;
-	@Autowired
-	UtilityBillsDetailService utilityDetailService;
-	@Autowired
-	FeedbackRepository feedbackRepository;
-	@Autowired
-	UtilityBillsRepository utilityBillsRepository;
-	@Autowired
-	private FiscalYearDetailService fiscalYearDetailService;
 	
 	@Autowired
 	private SportsDetailService sportsDetailService;
 	
-	@Autowired
-	private InsuranceCustomerRegisterDetailService insCustRegisterDetailService;
-	@Autowired
-	private InsuranceCompanyDetailService insuranceCompanyDetailService;
-	@Autowired
-	private InsuranceSchemaDetailService insuranceSchemaDetailService;
-	@Autowired
-	private GoodForPaymentDetailService goodForPaymentDetailService;
-	@Autowired
-	private InsuranceCustomerRepository insuranceCustomerRepository;
-	@Autowired
-	private ManagerChequeDetailService managerChequeDetailService;
-	
-	@Autowired
-	private LockerLogDetailService lockerLogDetailService;	
-	
-	@Autowired
-	private ErrorIssueLogDetailService errorIssueLogDetailService;
-	
-	@Autowired
-	LockerLogRepository lockerLogRepository;
-	
-	@Autowired
-	BudgetDetailService budgetDetailService;
-	
-	@Autowired
-	private CardDetailService cardDetailService;
-	@Autowired
-	private CardRenewDetailService cardRenewDetailService;
-	
-	@Autowired
-	private CardDeliveryDetailService cardDeliverDetailService;
-	
-	@Autowired
-	private CardRepository cardRepository;
-	@Autowired
-	private CardRenewRepository cardRenewRepository;
-	@Autowired
-	private MaintenanceLogRepository maintenanceLogRepository;
-	@Autowired
-	private MaintenanceLogDetailService maintenanceLogDetailService;
-	@Autowired
-	private LockerDetailService lockerDetailService;
-	@Autowired
-	private AccountInfoDetailService accountInfoDetailService;
 
 	@RequestMapping(value = "/getsports", method = RequestMethod.GET)
 	public List<Sports> getSports() {
